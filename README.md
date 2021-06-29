9be2cf3f3c97b9bb4c7000e721ef0d6716a2ff5f
Kim Fisher <kfisher@wcs.org>
2021-06-28 21:24:56 -0400

    flatten_place_sdrs.py initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # welikia-production
 Repository for scripts and tools for producing final Welikia assets, datasets, and texts
+
+## flatten_place_sdrs 
+This script parses the fields of its one required input `shapefile`. For any field names that have an underscore (`_`) followed by an integer, if the `score` value for that row/field is 1, 2, or 3, a tuple is created with a unique combination of `id_placename` and `id_sdr`. The tuples and their accompanying scores are then output to csv and can optionally be used to write directly to a Welikia database.
+
+### requirements
+This script is meant to be run within a Python environment installed by esri, i.e. one that contains `arcpy`, typically installed with a path like `
+C:\Python27\ArcGIS10.5\`.
+
+Additional required libraries may be installed with `/path/to/pip install -r requirements.txt`, ensuring that the correct `pip` in the correct environment is selected.
+
+To run database updates, an `.env` file should be created in the root of the repository, with a redacted structure like:
+```
+DBHOST=  
+DBPORT=  
+DBNAME=  
+DBUSER=  
+DBPASS=  
+DBTABLE=
+```
+In addition, to reach the production Welikia database, the IP of the machine running the script must be added to the relevant AWS whitelist. 
+
+### using the script
+Call the script with the correct Python interpreter normally, passing the required shapefile as the first argument. The shapefile *must* have a `Placename` field containing the integer `id_placename` for each feature.
+
+A csv will be output in the same location with same base name as the shapefile, unless an optional `-c --csv` path argument is supplied.
+
+If a `-d --database` argument is supplied, the placename/sdr/score data will be written to the `placename_location` table of the database. An `UPDATE` will be used if the placename/sdr combination already exists, and the score text will be appended; if the combination does not exist, an `INSERT` (with `canonical = TRUE`) will be used.
+
+```
+usage: flatten_place_sdrs.py [-h] [-d] [-c CSV] shapefile
+
+positional arguments:
+  shapefile          Path to input placename features shapefile
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -d, --database     Write location records to database directly, in addition
+                     to outputting csv. To use this option, database
+                     credentials must be specified in a config file as
+                     detailed in the README. (default: False)
+  -c CSV, --csv CSV  Path to output csv. If not specified, shapefile basename
+                     and location will be used. (default: None)
+```
+
+### other notes
+- If multiple shapefile records with the same placename/SDR combination (but potentially different scores) exist, only the first will be used.
+- This script should only be used once per shapefile to update the database; additional runs will cause updates to append multiple copies of the score text.
+- Database inserts set `canonical = 1`; updates do nothing with `canonical` (since there is no way to know which should be canonical).
---
+++ src/flatten_place_sdrs.py
@@ -0,0 +1,168 @@
+import arcpy
+import argparse
+import csv
+from pathlib2 import Path
+
+
+PLACENAME_FIELD = "Placename"
+SCORES = {
+    1: "[This feature is shown in this source but in a different place than the synthesized data.]",
+    2: "[This feature is shown in the synthesized data similar to this source.]",
+    3: "[This feature is shown in the synthesized data based on this source.]",
+}
+
+
+def extant_shp(string):
+    if Path(string).is_file():
+        return string
+    else:
+        raise
+
+
+def get_sdr_id(field):
+    field_parts = field.split("_")
+    if len(field_parts) > 1:
+        try:
+            return int(field_parts[-1])
+        except ValueError as e:
+            pass  # not numeric
+    return None
+
+
+def sdr_fields(fields):
+    return_fields = []
+    for field in fields:
+        sdr_id = get_sdr_id(field)
+        if sdr_id:
+            return_fields.append(field)
+    return return_fields
+
+
+def get_rowcount(cursor, query):
+    cursor.execute(query)
+    cursor.fetchall()
+    return cursor.rowcount
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "shapefile", type=extant_shp, help="Path to input placename features shapefile"
+)
+parser.add_argument(
+    "-d",
+    "--database",
+    action="store_true",
+    default=False,
+    help="Write location records to database directly, in addition to outputting csv. "
+    "To use this option, database credentials must be specified in a config file as detailed in the README.",
+)
+parser.add_argument(
+    "-c",
+    "--csv",
+    help="Path to output csv. If not specified, shapefile basename and location will be used.",
+)
+options = vars(parser.parse_args())
+shapefile = options.get("shapefile")
+csvfile = options.get("csv") or str(Path(shapefile).with_suffix(".csv"))
+
+if options["database"]:
+    import mysql.connector
+    from dotenv import dotenv_values
+
+    envfile = "{}/.env".format(Path(__file__).parents[1].as_posix())
+    config = dotenv_values(dotenv_path=envfile, verbose=True)
+    DBHOST = config.get("DBHOST")
+    DBPORT = config.get("DBPORT")
+    DBNAME = config.get("DBNAME")
+    DBUSER = config.get("DBUSER")
+    DBPASS = config.get("DBPASS")
+    DBTABLE = config.get("DBTABLE")
+    if (
+        not DBHOST
+        or not DBPORT
+        or not DBNAME
+        or not DBUSER
+        or not DBPASS
+        or not DBTABLE
+    ):
+        raise ImportError("Missing database configuration parameters")
+
+    conn = mysql.connector.connect(
+        host=DBHOST, port=DBPORT, database=DBNAME, user=DBUSER, password=DBPASS
+    )
+    mysql_cursor = conn.cursor()
+
+fields = [PLACENAME_FIELD] + sdr_fields([f.name for f in arcpy.ListFields(shapefile)])
+if len(fields) <= 1:
+    exit("No SDR fields to process.")
+
+placename_sdr_rows = []
+seen = set()
+with open(csvfile, "w") as csvhandle:
+    csvwriter = csv.writer(csvhandle, lineterminator="\n")
+    csvwriter.writerow(["id_placename", "id_sdr", "location"])
+
+    with arcpy.da.SearchCursor(shapefile, fields) as cursor:
+        for row in cursor:
+            id_placename = int(row[0])
+            for position, field in enumerate(fields):
+                id_sdr = get_sdr_id(field)
+                score = row[position]
+                if score in SCORES.keys():
+                    placename_sdr_row = (id_placename, id_sdr)
+                    # If the same place/sdr combination occurs twice with different scores,
+                    # e.g. (96,191,2) and (96,191,1), only the first entry will be written
+                    if placename_sdr_row not in seen:
+                        seen.add(placename_sdr_row)
+                        placename_sdr_rows.append(placename_sdr_row + (score,))
+
+    csvwriter.writerows(placename_sdr_rows)
+
+if options["database"]:
+    missing_placenames = []
+    missing_sdrs = []
+    for locationrow in placename_sdr_rows:
+        id_placename = locationrow[0]
+        id_sdr = locationrow[1]
+
+        placename_query = "SELECT id_placename FROM placename WHERE id_placename = {}".format(
+            id_placename
+        )
+        pncount = get_rowcount(mysql_cursor, placename_query)
+        if pncount < 1:
+            missing_placenames.append(id_placename)
+
+        sdr_query = "SELECT id FROM sdr WHERE id = {}".format(id_sdr)
+        sdrcount = get_rowcount(mysql_cursor, sdr_query)
+        if sdrcount < 1:
+            missing_sdrs.append(id_sdr)
+
+        if pncount > 0 and sdrcount > 0:
+            location_query = "SELECT * FROM {} WHERE id_placename = {} AND id_sdr = {}".format(
+                DBTABLE, id_placename, id_sdr
+            )
+            locationcount = get_rowcount(mysql_cursor, location_query)
+            if locationcount > 0:
+                query = (
+                    "UPDATE {} SET location = CONCAT_WS(CHAR(13), location, '{}') WHERE "
+                    "id_placename = {} AND id_sdr = {}".format(
+                        DBTABLE, SCORES[locationrow[2]], id_placename, id_sdr
+                    )
+                )
+            else:
+                query = (
+                    "INSERT INTO {} (id_placename, id_sdr, location, canonical) "
+                    "VALUES ({}, {}, '{}', 1)".format(
+                        DBTABLE, id_placename, id_sdr, SCORES[locationrow[2]]
+                    )
+                )
+            print(query)
+            mysql_cursor.execute(query)
+            conn.commit()
+
+    if len(missing_placenames) > 0:
+        print("missing placename ids:")
+        print("\n".join([str(p) for p in missing_placenames]))
+    if len(missing_sdrs) > 0:
+        print("missing SDR ids:")
+        print("\n".join([str(s) for s in missing_sdrs]))
